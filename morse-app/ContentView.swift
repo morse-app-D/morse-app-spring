389f93454a3cabce3bce55cdc3618c17ad8d531f
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-25 21:51:58 +0900

    first commit

---
+++ morse-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  morse-app
+//
+//  Created by 大澤清乃 on 2024/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ morse-app/morse_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  morse_appApp.swift
+//  morse-app
+//
+//  Created by 大澤清乃 on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct morse_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
