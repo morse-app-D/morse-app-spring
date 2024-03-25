d8d1df89c32f37ba341fecbfb86542b8ce8a1ae3
HikaruHonda <hikaru.h.developer@gmail.com>
2024-03-26 00:48:10 +0900

    Initial Commit

---
+++ spring-canp2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spring-canp2024
+//
+//  Created by 本田輝 on 2024/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spring-canp2024/spring_canp2024App.swift
@@ -0,0 +1,17 @@
+//
+//  spring_canp2024App.swift
+//  spring-canp2024
+//
+//  Created by 本田輝 on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct spring_canp2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
