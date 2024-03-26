888deee4bf7220cded3a991fb9b5e7952dfcb263
kiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-03-26 17:29:41 +0900

    add: ホーム画面の作成とmodel一旦作ったよ~~~~~

--- morse-app/ContentView.swift
+++ morse-app/ContentView.swift
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            HomeView()
+                .tabItem {
+                    Image(systemName: "circle")
+                }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Color {
+    static let backColor = Color("BackColor")
 }
