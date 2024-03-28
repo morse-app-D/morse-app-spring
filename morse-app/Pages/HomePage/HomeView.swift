fc7f405def028e7157265880f1a36ef9bd0bb867
aoba320 <aoba.suzuki7321@gmail.com>
2024-03-28 16:58:52 +0900

    .

--- morse-app/Pages/HomePage/HomeView.swift
+++ morse-app/Pages/HomePage/HomeView.swift
@@ -55,7 +55,68 @@ struct Cassette: View {
     }
 }
 
+struct ModalView: View {
+
+    @State var name: String = ""
+    @State var editorText: String = ""
+
+    @Binding var isPresented: Bool
+
+    let characterLimit = 30
+
+    var body: some View {
+        VStack {
+            TextField("送り先を入力", text: $name)
+                .background(Color.white)
+                .cornerRadius(12)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(lineWidth: 1)
+                )
+                .padding(.horizontal, 20)
+                .font(.system(size: 24))
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(TextAlignment.center)
+                .frame(width: 380)
+
+            Text("メッセージをひらがなで入力しよう！")
+                .padding(.top, 30)
+                .font(.system(size: 20))
+                .foregroundColor(.white)
+
+            TextEditor(text: $editorText)
+                .frame(width: 340, height: 200)
+                .scrollContentBackground(.hidden)
+                .background(.white, in: RoundedRectangle(cornerRadius: 12))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                        .stroke(lineWidth: 1)
+                )
+
+                .padding(.bottom, 10)
+                .multilineTextAlignment(.center)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .onChange(of: editorText) { newValue in
+                                    if newValue.count > characterLimit {
+                                        editorText = String(newValue.prefix(characterLimit))
+                                    }
+                                }
+
+            Button("Done") {
+                isPresented = false
+            }
+            .font(.system(size: 30))
+            .padding()
+            .foregroundColor(.white)
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(red: 0.88, green: 0.84, blue: 0.79))
+    }
+}
+
 struct TopButton: View {
+    @State private var showModal = false
     @State private var toProfileView = false
     @State private var toSendView = false
     var body: some View {
@@ -72,12 +133,16 @@ struct TopButton: View {
             })
             Button(action: {
                 self.toSendView = true
+                showModal.toggle()
             }, label: {
                 Image(systemName: "square.and.pencil.circle")
             })
             .font(.system(size: 40))
             .foregroundColor(.white)
             .padding(.leading, 240)
+            .sheet(isPresented: $showModal) {
+                ModalView(isPresented: $showModal)
+            }
 //            .sheet(isPresented: $toSendView, content: {
 //                SendView()
 //            })
